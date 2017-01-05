d2e9dc9b1978c6b3476c229a842bd0df8b03a048
G03-EQ05\tarde <esesito@gmail.com>
2017-01-05 18:02:22 +0100

    Figuras con herencia y metodo para heredar

---
+++ 02_js/18_figuras_poo/18_figuras.js
@@ -0,0 +1,98 @@
+var Figura = function(padreDom, nombreFigura) {
+
+    if (typeof padreDom == "undefined")
+        return;
+    // Añadimos el FORM al padreDom
+    this.formCalc = document.createElement("form");
+    padreDom.appendChild(this.formCalc);
+    // Y los botones
+    this.alto = CreadorDOM.addInput(this.formCalc, "alto", "alto");
+    this.ancho = CreadorDOM.addInput(this.formCalc, "ancho", "ancho");
+    this.resultado = CreadorDOM.addInput(this.formCalc, "Resultado", "resultado");
+    // El botón de calcular
+    CreadorDOM.addButton(this, " Calcular Área " + nombreFigura, this.calculaArea);
+    // Y la caja con el resultado
+}
+
+Figura.prototype.metodoCompartido = function(frase) {
+    alert("Metodo Compartido " + frase);
+}
+
+function HeredarFigura(funCalculaArea, nombre) {
+    var nombreFun = nombre;
+    var funcionConstructora = function(padreDom) {
+        Figura.call(this, padreDom, nombreFun);
+    }
+
+    funcionConstructora.prototype = new Figura();
+    funcionConstructora.prototype.calculaArea = funCalculaArea;
+    return funcionConstructora;
+}
+/*
+var Rectangulo = function(padreDom) {
+    Figura.call(this, padreDom);
+}
+Rectangulo.prototype = new Figura();
+
+Rectangulo.prototype.calculaArea = function() {
+    this.resultado.value = parseInt(this.alto.value) *
+        parseInt(this.ancho.value);
+}
+
+var Triangulo = function(padreDom) {
+    Figura.call(this, padreDom);
+}
+Triangulo.prototype = new Figura();
+
+Triangulo.prototype.calculaArea = function() {
+    this.resultado.value = parseInt(this.alto.value) *
+        parseInt(this.ancho.value) / 2;
+}
+
+var Elipse = function(padreDom) {
+    Figura.call(this, padreDom);
+}
+Elipse.prototype = new Figura();
+
+Elipse.prototype.calculaArea = function() {
+    this.resultado.value = (parseInt(this.alto.value)/2 *
+        parseInt(this.ancho.value) / 2) * Math.PI;
+}*/
+
+
+
+calculaAreaRectangulo = function() {
+    this.resultado.value = parseInt(this.alto.value) *
+        parseInt(this.ancho.value);
+    this.metodoCompartido("AAAA");
+}
+
+calculaAreaTriangulo = function() {
+    this.resultado.value = parseInt(this.alto.value) *
+        parseInt(this.ancho.value) / 2;
+    this.metodoCompartido("EEEE");
+}
+
+calculaAreaElipse = function() {
+    this.resultado.value = (parseInt(this.alto.value) / 2 *
+        parseInt(this.ancho.value) / 2) * Math.PI;
+    this.metodoCompartido("OOOO");
+}
+
+var Rectangulo = HeredarFigura(calculaAreaRectangulo, "Rectangulo");
+var Triangulo = HeredarFigura(calculaAreaTriangulo, "Triangulo");
+var Elipse = HeredarFigura(calculaAreaElipse, "Elipse");
+
+
+/*
+var Trapecio = function(padreDom) {
+    Figura.call(this, padreDom);
+    this.baseMayor = CreadorDOM.addInput(this.formCalc, "Base Mayor", "base_mayor");
+
+}
+Trapecio.prototype = new Figura();
+
+Trapecio.prototype.calculaArea = function() {
+    this.resultado.value = ((parseInt(this.ancho.value) + parseInt(this.baseMayor.value)) / 2) *
+        parseInt(this.alto.value);
+}*/
