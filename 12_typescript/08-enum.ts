822cfc487b666ca5287d910f0c4469d8d4b73bfd
G03-EQ05\tarde <esesito@gmail.com>
2017-02-22 20:57:38 +0100

    Ejemplo Angular 2 - Room & Customer

--- 12_typescript/08-enum.ts
+++ 12_typescript/08-enum.ts
@@ -1,25 +1,25 @@
-enum TipoDeVia{
+enum TipoDeVia {
     Calle = 5, //solo con numeros enteros
-    Plaza = 3, 
+    Plaza = 3,
     Camino, //Valdria 4, el anterior + 1
     Avenida, //Valdria 5, el anterior + 1
     Carretera //Vale 6
 }
 
-console.log("Calle "+TipoDeVia.Calle);
-console.log("Plaza "+TipoDeVia.Plaza);
-console.log("Camino "+TipoDeVia.Camino);
-console.log("Avenida "+TipoDeVia.Avenida);
-console.log("Carretera "+TipoDeVia.Carretera);
+console.log("Calle " + TipoDeVia.Calle);
+console.log("Plaza " + TipoDeVia.Plaza);
+console.log("Camino " + TipoDeVia.Camino);
+console.log("Avenida " + TipoDeVia.Avenida);
+console.log("Carretera " + TipoDeVia.Carretera);
 
 namespace TipoDeVia {
-    export function esAvenida(texto: string): TipoDeVia{
-        if(texto == "Avda")
+    export function esAvenida(texto: string): TipoDeVia {
+        if (texto == "Avda")
             return TipoDeVia.Avenida;
     }
 }
 
-var miTipoDeVia : TipoDeVia = TipoDeVia.Calle;
+var miTipoDeVia: TipoDeVia = TipoDeVia.Calle;
 
 console.log("Tipo de via (num): " + miTipoDeVia);
 
